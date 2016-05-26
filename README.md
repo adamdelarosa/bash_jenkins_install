b53e58380762fb27fa81290f592be7f85ccd1c04
adamdelarosa <gojiradam@gmail.com>
2016-05-26 13:54:47 +0300

    bash

---
+++ README.md
@@ -0,0 +1,1 @@
+# bash_jenkins_install
---
+++ bash-jenkins-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+/etc/init.d/jenkins status
+/etc/init.d/jenkins start
+
